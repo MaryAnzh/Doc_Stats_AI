7afe096baaed185e729b27604f34dcb1ef237567
MaryAnzh <maryang@mail.ru>
2026-08-20 21:33:33 +0300

    feat: add header component, app main styles

---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  input,
+  inject,
+  computed,
+  output
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import type { ThemeType } from '../../shared';
+import { THEMES } from '../../shared';
+const { LIGHT, DARK } = THEMES;
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  private router = inject(Router);
+
+  theme = input<ThemeType>();
+  toggleTheme = input<() => void>();
+  lightTheme = LIGHT;
+  darkTheme = DARK;
+
+  handleTheme() {
+    this.toggleTheme()?.();
+  }
+}
