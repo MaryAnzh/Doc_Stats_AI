0ff0ce7792fa65a6fc2039cd361cc5be8a14e151
MaryAnzh <maryang@mail.ru>
2026-08-23 15:13:42 +0300

    Updates

---
+++ assets/logo.stories-CGMrKGeV.js
@@ -0,0 +1,16 @@
+import{i as e}from"./preload-helper-CWn0lyrb.js";import{i as t,n,r,t as i}from"./logo-Ggcr5ZHY.js";var a,o,s,c,l,u,d;e((()=>{n(),r(),{DEFAULT:a,SM:o}=t,s={title:`UI/Logo`,component:i,argTypes:{showText:{control:`boolean`,description:`Show logo test`},size:{control:`select`,options:[a,o],description:`Logo size`}}},c={args:{showText:!0,size:a}},l={args:{showText:!0,size:o}},u={args:{showText:!1,size:a}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    showText: true,
+    size: DEFAULT
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    showText: true,
+    size: SM
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    showText: false,
+    size: DEFAULT
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Default`,`Small`,`IconOnly`]}))();export{c as Default,u as IconOnly,l as Small,d as __namedExportsOrder,s as default};
---
+++ assets/react-S1eeuDuK.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-JROSPFPF-4Krud1WZ.js";t();export{e as MDXProvider};
